9cde53c63e275a93388778f4d6c92dc0b8b4dfb0
alannguyen00 <58610714+alannguyen00@users.noreply.github.com>
2020-02-10 14:03:25 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let name = game.askForString("What is your name")
+
+let favorite_color = game.askForString("What is yur favorite color")
+
+game.splash("Your name is:" + name)
+
+game.splash("Your name is:" + name + "Your favrite color is:" + favorite_color) 
